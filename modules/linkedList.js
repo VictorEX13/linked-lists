53d1165e6c87a9c98668065c65dbc1565a722437
Victor <victorshiira4869@gmail.com>
2023-03-21 13:11:50 -0300

    Create node append and prepend methods on LinkedList Factory

--- modules/linkedList.js
+++ modules/linkedList.js
@@ -1,3 +1,35 @@
-const LinkedList = () => {};
+import ListNode from "./listNode";
+
+const LinkedList = () => {
+  let list = {};
+
+  const append = (value) => {
+    if (Object.keys(list).length === 0) {
+      list.value = value;
+      list.next = null;
+    } else {
+      const newNode = ListNode(value);
+
+      let listCopy = { ...list };
+      while (listCopy.next) {
+        listCopy = listCopy.next;
+      }
+
+      listCopy.next = newNode;
+    }
+  };
+
+  const prepend = (value) => {
+    if (Object.keys(list).length === 0) {
+      list.value = value;
+      list.next = null;
+    } else {
+      const newNode = ListNode(value, list);
+      list = newNode;
+    }
+  };
+
+  return { append, prepend };
+};
 
 export default LinkedList;
--- modules/listNode.js
+++ modules/listNode.js
@@ -1,3 +1,5 @@
-const ListNode = (value = null, nextNode = null) => {};
+const ListNode = (value = null, next = null) => {
+  return { value, next };
+};
 
 export default ListNode;
